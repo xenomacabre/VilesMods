c4a9ff5d3fb78ff8b71f941aee7c347977884120
TheLoneTec <kaceysherrard@hotmail.com>
2023-10-18 19:49:23 +0100

    correction

--- Vile REQUISITE - communityframework/Source/communityframework/communityframework/Harmony patches/AutoLinks.cs
+++ Vile REQUISITE - communityframework/Source/communityframework/communityframework/Harmony patches/AutoLinks.cs
@@ -10,47 +10,51 @@ using UnityEngine;
 
 namespace CF
 {
-    [ClassWithPatches("AutoLinksForUnlock")]
+    [StaticConstructorOnStartup]
     static class AutoLinksForUnlockPatch
     {
         /// <summary>
-        /// This add recipe links automatically if AutoLinks is present
+        /// This add recipe links automatically if AutoLinks is present.
         /// </summary>
-        [HarmonyPatch(typeof(Autolinks.Autolinks), "ProcessThing")]
-        class AutoLinks_ProcessThing
+        /// 
+        static AutoLinksForUnlockPatch()
         {
-            public static void Postfix(ThingDef def, Autolinks.Autolinks __instance)
+            foreach (ThingDef allDef in DefDatabase<ThingDef>.AllDefs)
+                ProcessThing(allDef);
+        }
+
+
+        public static void ProcessThing(ThingDef def)
+        {
+            CompProperties_UnlocksRecipe unlockRecipes = def.GetCompProperties<CompProperties_UnlocksRecipe>();
+            if (unlockRecipes != null && !unlockRecipes.recipes.NullOrEmpty())
             {
-                CompProperties_UnlocksRecipe unlockRecipes = def.GetCompProperties<CompProperties_UnlocksRecipe>();
-                if (unlockRecipes != null && !unlockRecipes.recipes.NullOrEmpty())
+                foreach (RecipeDef recipe in unlockRecipes.recipes)
                 {
-                    foreach (RecipeDef recipe in unlockRecipes.recipes)
+                    foreach (IngredientCount ingredient in DefDatabase<RecipeDef>.AllDefsListForReading.Find(r => r.defName == recipe.defName).ingredients)
                     {
-                        foreach (IngredientCount ingredient in DefDatabase<RecipeDef>.AllDefsListForReading.Find(r => r.defName == recipe.defName).ingredients)
-                        {
-                            if (ingredient.IsFixedIngredient)
-                                AddMulti((Def)def, (Def)ingredient.FixedIngredient);
-                        }
+                        if (ingredient.IsFixedIngredient)
+                            AddMulti((Def)def, (Def)ingredient.FixedIngredient);
                     }
                 }
             }
+        }
 
-            private static void AddLink(Def def, Def toAdd)
-            {
-                if (def == null || toAdd == null)
-                    return;
-                if (def.descriptionHyperlinks == null)
-                    def.descriptionHyperlinks = new List<DefHyperlink>();
-                if (def.descriptionHyperlinks.Any<DefHyperlink>((Predicate<DefHyperlink>)(x => x.def == toAdd)))
-                    return;
-                def.descriptionHyperlinks.Add((DefHyperlink)toAdd);
-            }
+        private static void AddLink(Def def, Def toAdd)
+        {
+            if (def == null || toAdd == null)
+                return;
+            if (def.descriptionHyperlinks == null)
+                def.descriptionHyperlinks = new List<DefHyperlink>();
+            if (def.descriptionHyperlinks.Any<DefHyperlink>((Predicate<DefHyperlink>)(x => x.def == toAdd)))
+                return;
+            def.descriptionHyperlinks.Add((DefHyperlink)toAdd);
+        }
 
-            private static void AddMulti(Def def, Def toAdd)
-            {
-                AddLink(def, toAdd);
-                AddLink(toAdd, def);
-            }
+        private static void AddMulti(Def def, Def toAdd)
+        {
+            AddLink(def, toAdd);
+            AddLink(toAdd, def);
         }
     }
 }
